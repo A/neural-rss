2b98bc7537df279c46168b888ec3071562e9a88d
Shuvalov Anton <anton_shuvalov@me.com>
2013-09-14 12:06:00 +0600

    Broken state

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 'use strict';
 
+var nconf = require('nconf');
 var data = require('./data');
+nconf.file({ file: 'config.json' });
+
+console.log(nconf.get('user'));
+
 
 var natural = require('natural'),
   porterStemmer = natural.PorterStemmerRu,
@@ -9,18 +14,19 @@ var natural = require('natural'),
 
 for (var i = 0; i < data.good.length; i++) {
   classifier.addDocument(data.good[i], 'good');
-  console.log('learn good')
+  console.log('learn good');
 };
 
 for (var i = 0; i < data.bad.length; i++) {
   classifier.addDocument(data.bad[i], 'bads');
-  console.log('learn bad')
+  console.log('learn bad');
 };
 
 classifier.train();
 
 console.log('= = = = = = = = = =');
 console.log('START CLASSIFICATION');
+
 console.log('Test on good');
 for (var i = 0; i < data.test_good.length; i++) {
   console.log("> ",classifier.classify(data.test_good[i]));
@@ -31,29 +37,5 @@ for (var i = 0; i < data.test_bad.length; i++) {
   console.log("> ",classifier.classify(data.test_bad[i]));
 };
 
-
-
-
-return
-
-
-
-
-
-// for (var i = 0; i < data.bad.length; i++) {
-//   net.train(data.bad[i], 'bad');
-// };
-
-// for (var i = 0; i < data.good.length; i++) {
-//   net.train(data.good[i], 'good');
-// };
-
-// for (var i = 0; i < data.test_good.length; i++) {
-//   var cat = net.classify(data.test_good[i]);
-//   console.log(cat);
-// };
-
-// for (var i = 0; i < data.test_bad.length; i++) {
-//   var cat = net.classify(data.test_bad[i]);
-//   console.log(cat);
-// };
+var raw = JSON.stringify(classifier);
+console.log(raw)
---
+++ neural/index.js
@@ -0,0 +1,2 @@
+'use strict'; 
+var natural = require('natural');
